f477bb546ada2e6a4b6ae6647f307c5cb8555dbe
Damian Valencia <cvalencia19@ucol.mx>
2024-09-03 17:31:31 -0600

    Main character Added

--- src/player.py
+++ src/player.py
@@ -4,7 +4,7 @@ import pygame
 class Player:
     def __init__(self, game):
         self.screen = game.screen
-        self.image = pygame.image.load("assets\images\player\mario_1.png")
+        self.image = pygame.image.load("assets\images\player\Sonia_ 1.png")
         self.rect = self.image.get_rect()
         self.rect.center = game.screen.get_rect().center
 

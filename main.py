75ab3ab0f7c09a9421442f985f72c53a026b339e
Damian Valencia <cvalencia19@ucol.mx>
2024-09-03 16:28:08 -0600

    Main classes defined

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from src.settings import Settings
+from src.game import Game
+
+def main():
+    pygame.init()
+    settings = Settings()
+    game = Game(settings)
+    game.run()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ src/player.py
@@ -0,0 +1,12 @@
+
+import pygame
+
+class Player:
+    def __init__(self, game):
+        self.screen = game.screen
+        self.image = pygame.image.load("assets\images\player\mario_1.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = game.screen.get_rect().center
+
+    def draw(self):
+        self.screen.blit(self.image, self.rect)

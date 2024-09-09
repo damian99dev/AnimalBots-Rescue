00f7835567725f5046d13dec2dd889b328ecc711
damiXdev <cvalencia19@ucol.mx>
2024-09-09 09:57:05 -0600

    Resolituon was adjusted

--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,5 @@ class Settings:
     def __init__(self):
         self.screen_width = 1920
         self.screen_height = 1080
-        self.bg_color = (30, 30, 30)
+        self.bg_color = (31, 30, 30)
         self.fps = 60

0c23af5ebe32d57d8d7c43f4bfe21ef9221dd068
Gregorio Yahir Sánchez Zavaleta <gsanchez35@ucol.mx>
2024-11-09 15:49:15 -0600

    Avanze en el nivel 3, agregado sistema de optimizacion y correcion de la camara + resolucion

--- src/level.py
+++ src/level.py
@@ -8,19 +8,11 @@ from game_over import GameOverScreen, get_font  # Importar GameOverScreen y get_
 class Level:
     def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()
-
-        # Creamos grupos
         self.all_sprites = AllSprites()  # Grupo general para todos los sprites con manejo de zoom
         self.collision_sprites = pygame.sprite.Group()  # Grupo de sprites con colisiones
         self.background_objects = pygame.sprite.Group()  # Grupo para objetos de fondo (sin colisiones)
-
-        # Inicializamos el fondo
         self.background = None
-
-        # Lista para almacenar todas las instancias de 'fin'
         self.fin_objects = []
-
-        # Configuramos el nivel
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
@@ -30,11 +22,9 @@ class Level:
             self.background = pygame.Surface((tmx_map.width * tile_size, tmx_map.height * tile_size))
             for x, y, surf in backg_layer.tiles():
                 self.background.blit(surf, (x * tile_size, y * tile_size))
-
         # Cargamos la capa de suelo 'Suelo'
         for x, y, surf in tmx_map.get_layer_by_name('Suelo').tiles():
             Sprite((x * tile_size, y * tile_size), surf, (self.all_sprites, self.collision_sprites))
-
         # Cargamos la capa de objetos 'Objetos' (protagonista, meta y fin)
         for obj in tmx_map.get_layer_by_name('Objetos'):
             if obj.name == 'player':
@@ -49,7 +39,6 @@ class Level:
                 fin_image = pygame.image.load('graphics/Background/ded.png').convert_alpha()
                 fin_image = pygame.transform.scale(fin_image, (obj.width, obj.height))
                 self.fin_objects.append((fin_rect, fin_image, (obj.x, obj.y)))
-
         # Cargar los objetos de la capa 'Backgtras' (por ejemplo, árboles)
         self.load_background_objects(tmx_map)
 
@@ -58,8 +47,7 @@ class Level:
         backgtras_layer = tmx_map.get_layer_by_name('Backgtras')
         if backgtras_layer:
             for obj in backgtras_layer:
-                if obj.name in ['arbol', 'mig', 'jos', 'dam', 'gre', 'rox', 'nao', 'flechaa', 'flechab', 'derecha', 'arbu', 'arbust', 'rocab', 'rocag', 'pa', 'st']:
-                    # Ruta a las imágenes correspondientes para cada objeto
+                if obj.name in ['arbol', 'mig', 'jos', 'dam', 'gre', 'rox', 'nao', 'flechaa', 'flechab','pinchos', 'negro',  'derecha', 'arbu', 'arbust', 'rocab', 'rocag', 'pa', 'st', 'laser', 'laser2', 'laser3']:
                     image_path = f'graphics/Background/{obj.name}.png'  # Asegúrate de tener las imágenes en la carpeta 'graphics/Background'
                     object_image = pygame.image.load(image_path).convert_alpha()
                     object_image = pygame.transform.scale(object_image, (obj.width, obj.height))
@@ -73,17 +61,14 @@ class Level:
         # Actualizamos y dibujamos los sprites con la cámara ajustada al jugador
         self.all_sprites.update(dt)
         self.all_sprites.draw(self.player.hitbox_rect.center, self.player)  # Aquí se usa el centro del jugador para centrar la cámara
-
         self.display_surface.blit(self.meta_image, self.meta_pos)
         for fin_rect, fin_image, fin_pos in self.fin_objects:
             self.display_surface.blit(fin_image, fin_pos)
-
         # Verificar si el jugador ha alcanzado la meta
         if self.player.hitbox_rect.colliderect(self.meta_rect):
             from victory import VictoryScreen
             victory_screen = VictoryScreen()
             victory_screen.run()  # Mostrar la pantalla de victoria
-
         # Verificar si el jugador ha alcanzado alguno de los objetos 'fin'
         for fin_rect, _, _ in self.fin_objects:
             if self.player.hitbox_rect.colliderect(fin_rect):

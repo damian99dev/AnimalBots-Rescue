3bdaac8ad5b0386f5122c4341f76c126df448e5a
Gregorio Yahir Sánchez Zavaleta <gsanchez35@ucol.mx>
2024-11-22 22:05:06 -0600

    actualizado nivel 3-1

--- src/enemy.py
+++ src/enemy.py
@@ -18,9 +18,17 @@ class Enemy(pygame.sprite.Sprite):
         self.animation_index = 0
         self.animation_speed = 0.1
 
+        # Ajustar el tamaño de la hitbox
+        self.hitbox_rect.size = (64, 64)
+        # Ajustar el tamaño de la hitbox de colisión con el jugador
+        self.player_hitbox_rect.size = (40, 40)
+
     def apply_gravity(self, dt):
         if not self.on_ground:
             self.direction.y += self.gravity * dt
+            self.rect.y += self.direction.y * dt
+            self.hitbox_rect.y = self.rect.y
+            self.player_hitbox_rect.y = self.rect.y
         else:
             self.direction.y = 0
 
@@ -73,9 +81,10 @@ class Enemy3(Enemy):
     def __init__(self, pos, images, collision_sprites):
         super().__init__(pos, images, collision_sprites)
         self.rect.size = (200, 200)
-        self.hitbox_rect.size = (180, 200)  # Ajustar el tamaño de la hitbox
-        self.player_hitbox_rect.size = (120, 125)  # Ajustar el tamaño de la hitbox de colisión con el jugador
-        self.jump_force = -500  # Fuerza del salto, ajusta según sea necesario
+        self.hitbox_rect.size = (180, 190)  # Ajustar el tamaño de la hitbox
+        self.player_hitbox_rect.size = (120, 150)  # Ajustar el tamaño de la hitbox de colisión con el jugador
+        self.jump_force = -360  # Fuerza del salto, ajusta según sea necesario
+        self.speed = 150
 
     def jump(self):
         if self.on_ground:
@@ -101,5 +110,29 @@ class Enemy3(Enemy):
         # Variaciones específicas de Enemy3 en la animación
 
     def update(self, dt):
-        super().update(dt)
-        # Variaciones específicas de Enemy3 en la actualización
+        # Movimiento en el eje x
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        for sprite in self.collision_sprites:
+            if self.hitbox_rect.colliderect(sprite.rect):
+                if self.direction.x > 0:  # Moving right
+                    self.hitbox_rect.right = sprite.rect.left
+                elif self.direction.x < 0:  # Moving left
+                    self.hitbox_rect.left = sprite.rect.right
+                self.direction.x *= -1  # Cambiar de dirección en el eje x
+                self.facing_right = not self.facing_right  # Cambiar el estado de dirección
+
+        # Aplicar gravedad
+        self.apply_gravity(dt)
+
+        # Movimiento en el eje y
+        self.hitbox_rect.y += self.direction.y * dt
+        self.check_ground_collision()
+
+        # Actualizar la posición del rectángulo principal
+        self.rect.topleft = self.hitbox_rect.topleft
+
+        # Actualizar la posición del rectángulo de colisión con el jugador
+        self.player_hitbox_rect.center = self.hitbox_rect.center
+
+        # Animar el enemigo
+        self.animate(dt)
